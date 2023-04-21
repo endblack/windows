6052ce9d71a000f266063fc537b8f847bcf1cc00
Megah <75638685+endblack@users.noreply.github.com>
2023-04-21 20:50:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget https://github.com/endblack/windows/blob/main/ngrok-stable-linux-amd64.zip?raw=true -O ngrok.zip
 unzip ngrok.zip
-(echo "maxmax23";echo "maxmax23") | sudo passwd runner
+(echo "@Maxmax23";echo "@Maxmax23") | sudo passwd runneradmin
 wget https://raw.githubusercontent.com/endblack/windows/main/sshd_config -O sshd_config
 sudo apt install ssh
 sudo cp sshd_config /etc/ssh/sshd_config
